ecb61f8f6ff61f2c8da3e7642613ef89512fdc96
Chungyuen Li <lizoyu1@gmail.com>
2017-02-02 16:14:26 -0600

    6.1 - finished, not tested

--- 6.1.cpp
+++ 6.1.cpp
@@ -7,6 +7,7 @@
 
 #include<iostream>
 #include<Eigen/Dense>
+#include<Eigen/LU>
 using namespace std;
 using namespace Eigen;
 
@@ -14,6 +15,7 @@ VectorXd GeneClassifierNorm( MatrixXd trainData, VectorXd state,
                             int num_class, double point)
 {
     double K = num_class;
+    VectorXd lambda(K), l(K);
     for( int k = 0; k < K; ++k )
     {
         // set mean(temp)
@@ -33,15 +35,38 @@ VectorXd GeneClassifierNorm( MatrixXd trainData, VectorXd state,
 
         // set variance(temp)
         MatrixXd var;
+        for( int i = 0; i < state.size(); ++i )
+        {
+            if( state(i) == k + 1 )
+            {
+                VectorXd deviation = trainData.col( i ) - mean;
+                var += deviation * deviation.transpose();
+            }
+        }
+        var = var / sum_delta;
+
         // set prior(save)
+        lambda(k) = sum_delta / trainData.cols();
 
         // likelihoods(save)
-    
+        VectorXd deviation = x - mean;
+        l(k) = exp( -0.5*deviation.transpose()*var.inverse()*deviation) /
+        ( pow( 2*M_PI, round(trainData.rows/2) )*sqrt( var.determinant() ) );
     }
     // classify the new datapoint
+    double sum_lk;
+    for( int i = 0; i < lambda.size(); ++i )
+        sum_lk += l(i) * lambda(i);
+
+    VectorXd P(K);
     for( int k = 0; k< K; ++k )
-    {
-        
-    }
+        P(k) = l(k)*lambda(k) / sum_lk;
+    
+    return P;
+}
+
+int main()
+{
+    
     return 0;
 }
