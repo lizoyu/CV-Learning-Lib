6ee665821d31da70978ecf4e3ced8145563db393
Chungyuen Li <lizoyu1@gmail.com>
2017-02-05 11:33:24 -0600

    7.1 - initiate

---
+++ 7.1.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 7.1.cpp - EM for mixtures of Gaussians
+	> Author: Chungyuen Li
+	> Mail: lizoyu1@gmail.com
+	> Created Time: 2017年02月05日 星期日 11时19分10秒
+ ************************************************************************/
+
+#include<iostream>
+#include<Eigen/Dense>
+using namespace std;
+using namespace Eigen;
+
+void EMforMoG( MatrixXd &trainData, int num_clusters )
+{
+    
+}
