3a1763dfb013df40a350a16fba8a5a21ac3f3fe6
Chungyuen Li <lizoyu1@gmail.com>
2017-02-06 11:20:41 -0600

    7.1 - initiate

--- 7.1.cpp
+++ 7.1.cpp
@@ -12,5 +12,25 @@ using namespace Eigen;
 
 void EMforMoG( MatrixXd &trainData, int num_clusters )
 {
-    
+    // lambda: (K,1); mean: (trainData.rows(),K); var:(rows*K,K);
+    // initiate
+    VectorXd lambda(K);
+    lambda += 1/K;
+    MatrixXd mean(trainData.rows(),K);
+    for( int k = 0; k < K; ++k )
+    {
+        for( int i = 0; i < trainData.rows(); ++i )
+            mean(i,k) = trainData.row(i).head(k+1) / (k+1);
+    }
+    MatrixXd var(trainData.rows()*K,trainData.rows());
+    VectorXd mean_all(trainData.rows());
+    for( int i = 0; i < mean_all.size(); ++i )
+        mean_all(i) = trainData.row(i).mean();
+    for( int i = 0; i < trainData.cols(); ++i )
+    {
+        for( int j = 0; j < trainData.rows(); ++j )
+            var(j,j) += pow( trainData(j,i) - mean_all(j), 2 );
+    }
+
+    // E-step
 }
