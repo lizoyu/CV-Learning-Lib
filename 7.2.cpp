04cb5c3e781e37097b26c90bdde21d15929cd3e1
Chungyuen Li <lizoyu1@gmail.com>
2017-02-20 15:01:24 -0600

    7.2 - finished E-step expect 'degree of freedom'

--- 7.2.cpp
+++ 7.2.cpp
@@ -11,7 +11,10 @@
 #include<Eigen/Dense>
 #include<Eigen/QR>
 #include<Eigen/Eigenvalues>
+#include<boost/math/special_functions/digamma.hpp>
 using namespace std;
+using namespace Eigen;
+using namespace boost::math;
 
 void MLforT( MatrixXd &trainData, VectorXd &mean, MatrixXd &var, double nu )
 {
@@ -19,21 +22,45 @@ void MLforT( MatrixXd &trainData, VectorXd &mean, MatrixXd &var, double nu )
     // initialize
     nu = 1000;
     mean = trainData.rowwise().mean();
-    for( i = 0; i < trainData.cols(); ++i )
+    for( int i = 0; i < trainData.cols(); ++i )
     {
         VectorXd deviation = trainData.col(i) - mean;
-        for( j = 0; j < trainData.rows(); ++j )
+        for( int j = 0; j < trainData.rows(); ++j )
             var(j,j) += pow( deviation(j), 2 );
     }
     
     // E-step
     VectorXd delta(trainData.cols()), E(delta.size()), E_log(E.size());
-    CompleteOrthogonalDecomposition<MatrixXd> cod( var );
-    for( i = 0; i < trainData.cols(); ++i )
+    
+    {CompleteOrthogonalDecomposition<MatrixXd> cod( var );
+    for( int i = 0; i < trainData.cols(); ++i )
     {
-        VectorXd deviation = trainData.cols(i) - mean;
+        VectorXd deviation = trainData.col(i) - mean;
         delta(i) = deviation.transpose() * cod.pseudoInverse() * deviation;
         E(i) = ( nu + trainData.rows() ) / ( nu + delta(i) );
-        E_log(i) = () - log10( nu/2 + delta(i)/2 );
+        E_log(i) = digamma( nu/2 + trainData.rows()/2 ) 
+            - log10( nu/2 + delta(i)/2 );
+    }}
+
+    // M-step
+    mean.Empty();
+    for( int i = 0; i < trainData.cols(); ++i )
+        mean += trainData.col(i) * E(i);
+    mean = mean / E.sum();
+    var.Empty();
+    for( int i = 0; i < trainData.cols(); ++i )
+    {
+        VectorXd deviation = trainData.col(i) - mean;
+        var += E(i) * deviation * deviation.transpose();
     }
+    var = var / E.sum();
+    // compute degree of freedom
+
+    // compute log likelihood
+    {CompleteOrthogonalDecomposition<MatrixXd> cod( var );
+    for( int i = 0; i < trainData.cols(); ++i )
+    {
+        VectorXd deviation = trainData.col(i) - mean;
+        delta(i) = deviation.transpose() * cod.pseudoInverse() * deviation;
+    }}
 }
