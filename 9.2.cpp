9b84ff9f30eab88ab076da2dee7155d2e626f825
Chungyuen Li <lizoyu1@gmail.com>
2017-03-17 11:13:27 -0500

    9.2 - initialize the header

---
+++ 9.2.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 9.2.cpp - Bayesian logistic
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年03月04日 星期六 09时16分03秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<Eigen/Dense>
+using namespace std;
+using namespace Eigen;
+
