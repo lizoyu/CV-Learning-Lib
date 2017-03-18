30a82ea02ae4bd65e2ec9fc2b906306ec1cf43b7
Chungyuen Li <lizoyu1@gmail.com>
2017-03-18 10:03:38 -0500

    9.1 - fixed 2 bugs

--- 9.1.cpp
+++ 9.1.cpp
@@ -9,6 +9,7 @@
 #include<cmath>
 #include<vector>
 #include<Eigen/Dense>
+#include<Eigen/QR>
 using namespace std;
 using namespace Eigen;
 
@@ -34,43 +35,50 @@ VectorXd MAPforLogi( MatrixXd &trainData, VectorXd &label, VectorXd &phi_init,
 	while( L - L_old > 0.001 )
 	{
 		// compute new estimate
-		phi = phi + H.inverse()*g;
+		CompleteOrthogonalDecomposition<MatrixXd> cod( H );
+		phi = phi + cod.pseudoInverse()*g;
 
 		// compute prediction, L, g, H
 		double y;
+		L_old = L;
 		for( int i = 0; i < label.size(); ++i )
 		{
 			y = 1 / (1 + exp(-phi.transpose()*data.col(i)));
-			if( label(i) == 1)
+			cout << "y: " << y << endl;
+			if( label(i) == 1 && y != 0 )
 				L -= log10(y);
-			else
+			else if( y!= 1 )
 				L -= log10(1-y);
+			cout << "L: " << L << endl;
 			g += (y - label(i)) * data.col(i);
 			H += y*(1-y)*data.col(i)*data.col(i).transpose();
 		}
-		
+		cout << "H:" << endl << H << endl;
+		cout << "g:" << endl << g << endl;
+		cout << "L: " << L << endl;
 	}
 	return phi;
 }
 
 int main()
 {
-	// two class, 1 - (10, 20, 30); 2 - (70, 80, 90);
+	// two class, 1 - (10, 20, 30); 0 - (70, 80, 90);
     MatrixXd trainData(3,6);
     trainData << 1.3, 0.9, 1.1, 7.4, 6.8, 7.1,
                  2.2, 1.8, 2.1, 8.2, 7.6, 8.1,
                  3.1, 2.4, 3.1, 9.1, 8.9, 9.1;
     trainData *= 10;
     VectorXd label(6);
-    label << 1, 1, 1, 2, 2, 2;
+    label << 1, 1, 1, 0, 0, 0;
     VectorXd point(3);
     point << 18, 28, 38;
 
     // learn the parameter phi
     VectorXd phi;
-    VectorXd phi_init = VectorXd::Random(trainData.cols()+1);
+    VectorXd phi_init = VectorXd::Random(trainData.rows()+1).array() + 1;
     double var_init = 1000;
     phi = MAPforLogi( trainData, label, phi_init, var_init );
 
+    cout << "Phi:" << endl << phi << endl;
 	return 0;
 }
