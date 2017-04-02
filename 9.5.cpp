410aa57e07e839ba1eaa37e20213549173c9d40b
Chungyuen Li <lizoyu1@gmail.com>
2017-04-02 09:16:23 -0500

    9.5 - debugging: equal likelihood

--- 9.5.cpp
+++ 9.5.cpp
@@ -6,17 +6,18 @@
 using namespace std;
 using namespace Eigen;
 
-double RVC( MatrixXd &trainData, VectorXd &label, double deg_free )
+MatrixXd RVC( MatrixXd &trainData, VectorXd &label, double deg_free,
+		VectorXd &mean, MatrixXd &var )
 {
-	MatrixXd HV = MatrixXd::Identity(trainData.cols(),trainData.cols());
+	MatrixXd data = MatrixXd::Ones(trainData.rows()+1,trainData.cols());
+	data.block(1, 0, trainData.rows(), trainData.cols()) = trainData;
+	MatrixXd HV = MatrixXd::Identity(data.cols(),data.cols());
 	MatrixXd HV_old = HV.array()*0.1;
 	while( abs(HV.diagonal().sum() - HV_old.diagonal().sum()) > 0.1 ){
 		HV_old = HV;
 		// MAP estimate of psi
 		// initialize
-		MatrixXd data = MatrixXd::Ones(trainData.rows()+1,trainData.cols());
-		data.block(1, 0, trainData.rows(), trainData.cols()) = trainData;
-		double L = -data.cols()*log10(2*M_PI*var_init)/2;
+		double L = -data.cols()*log10(2*M_PI*100)/2;
 		VectorXd g;
 		MatrixXd H;
 
@@ -31,11 +32,11 @@ double RVC( MatrixXd &trainData, VectorXd &label, double deg_free )
 			L_old = L;
 			for( int i = 0; i < label.size(); ++i )
 			{	
-				VectorXd kernel(trainData.cols());
+				VectorXd kernel(data.cols());
 				for( int j = 0; j < kernel.size(); ++j )
 				{
-					VectorXd minus = trainData.col(j) - trainData.col(i);
-					kernel(j) = exp(-0.5*(minus.transpose()*minus/1));
+					VectorXd minus = data.col(j) - data.col(i);
+					kernel(j) = exp(-0.5*minus.transpose()*minus);
 				}
 				y = 1/(1+exp(-psi.transpose()*kernel));
 				if( label(i) == 1 && y != 0 )
@@ -53,25 +54,25 @@ double RVC( MatrixXd &trainData, VectorXd &label, double deg_free )
 
 		// compute Hessian S
 		MatrixXd S = HV;
-		for( int i = 0 ; i < trainData.cols(); ++i )
+		for( int i = 0 ; i < data.cols(); ++i )
 		{
-			VectorXd kernel(trainData.cols());
+			VectorXd kernel(data.cols());
 			for( int j = 0; j < kernel.size(); ++j )
 			{
-				VectorXd minus = trainData.col(j) - trainData.col(i);
-				kernel(j) = exp(-0.5*(minus.transpose()*minus/1));
+				VectorXd minus = data.col(j) - data.col(i);
+				kernel(j) = exp(-0.5*minus.transpose()*minus);
 			}
 			double y = 1/(1+exp(-psi.transpose()*kernel));
 			S += y*(1-y)*kernel*kernel.transpose();
 		}
 
 		// Set mean and variance
-		VectorXd mean = psi;
+		mean = psi;
 		CompleteOrthogonalDecomposition<MatrixXd> cod( S );
-		MatrixXd var = -cod.pseudoInverse();
+		var = -cod.pseudoInverse();
 
 		// update diagonal of H(hidden variables)
-		for( int i = 0; i < trainData.cols(); ++i )
+		for( int i = 0; i < data.cols(); ++i )
 			HV(i,i) = ( 1 - HV(i,i)*var(i,i) + deg_free ) / 
 				( pow(mean(i),2) + deg_free );
 	}
@@ -88,9 +89,9 @@ double RVC( MatrixXd &trainData, VectorXd &label, double deg_free )
 			tracker.conservativeResize(tracker.size()-1);
 			mean.segment(k,mean.size()-1-k) = mean.tail(mean.size()-1-k);
 			mean.conservativeResize(mean.size()-1);
-			trainData.block(0,k,trainData.rows(),trainData.cols()-1-k) = 
-				trainData.rightCols(trainData.cols()-1-k);
-			trainData.conservativeResize(trainData.rows(),trainData.cols()-1);
+			data.block(0,k,data.rows(),data.cols()-1-k) = 
+				data.rightCols(data.cols()-1-k);
+			data.conservativeResize(data.rows(),data.cols()-1);
 			var.block(0,k,var.rows(),var.cols()-1-k) = 
 				var.rightCols(var.cols()-1-k);
 			var.conservativeResize(var.rows(),var.cols()-1);
@@ -98,7 +99,55 @@ double RVC( MatrixXd &trainData, VectorXd &label, double deg_free )
 				var.bottomRows(var.rows()-1-k);
 			var.conservativeResize(var.rows()-1,var.cols());
 		}
-		
 	}
+	return data;
+}
 
+VectorXd inferRVC( VectorXd &mean, MatrixXd &var, MatrixXd &trainData,
+					VectorXd &point )
+{
+	VectorXd point_p = VectorXd::Ones(point.size()+1);
+	point_p.tail(point.size()) = point;
+	VectorXd kernel(trainData.cols());
+	for( int j = 0; j < kernel.size(); ++j )
+	{
+		VectorXd minus = trainData.col(j) - point_p;
+		kernel(j) = exp(-0.5*minus.transpose()*minus);
+	}
+	double mean_a = mean.transpose()*kernel;
+	double var_a = kernel.transpose()*var*kernel;
+	double lambda = 1 / (1+exp(-mean_a/sqrt(1+M_PI*var_a/8)));
+	VectorXd l(2);
+	l(0) = lambda;
+	l(1) = 1 - lambda;
+	return l;
+}
+
+int main()
+{
+	// two class, 1 - (10, 20, 30); 0 - (70, 80, 90);
+    MatrixXd trainData(3,6);
+    trainData << 1.3, 0.9, 1.1, 7.4, 6.8, 7.1,
+                 2.2, 1.8, 2.1, 8.2, 7.6, 8.1,
+                 3.1, 2.4, 3.1, 9.1, 8.9, 9.1;
+    trainData *= 10;
+    VectorXd label(6);
+    label << 1, 1, 1, 0, 0, 0;
+    VectorXd point(3);
+    point << 18, 28, 38;
+
+    // learn
+    double deg_free = 3;
+    VectorXd mean;
+    MatrixXd var, data;
+    data = RVC( trainData, label, deg_free, mean, var );
+    cout << "mean:" << endl << mean << endl;
+    cout << "var:" << endl << var << endl;
+    cout << "data:" << endl << data << endl;
+
+    // predict
+    VectorXd l;
+    l = inferRVC( mean, var, data, point );
+    cout << "Likelihood:" << endl << l << endl;
+	return 0;
 }
